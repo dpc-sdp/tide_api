2f63c851d231ce34552cc73de16c3490acb78838
enzo - Eduardo Garcia <enzo@weknowinc.com>
2019-02-07 11:21:31 +1100

    Add validation for entity cache

--- src/Controller/TideApiController.php
+++ src/Controller/TideApiController.php
@@ -294,8 +294,11 @@ class TideApiController extends ControllerBase {
                   'json_response' => $json_response['data']['attributes'],
                   'uri' => $url->toUriString(),
                 ];
-                $this->cache('data')
-                  ->set($cid, $cached_route_data, Cache::PERMANENT, $cache_entity->getCacheTags());
+                // Validate if $cache_entity is not null
+                if($cache_entity) {
+                  $this->cache('data')
+                    ->set($cid, $cached_route_data, Cache::PERMANENT, $cache_entity->getCacheTags());
+                }
               }
               else {
                 unset($json_response['data']['type']);
