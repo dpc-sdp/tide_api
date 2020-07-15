1dc16d01f7b1086923a0c6d416652235565082f3
MdNadimHossain <getovic@gmail.com>
2020-07-15 16:18:01 +1000

    [SDPA-3936] Added function to process text that converts node link to path alias.

--- src/Plugin/jsonapi/FieldEnhancer/YamlEnhancer.php
+++ src/Plugin/jsonapi/FieldEnhancer/YamlEnhancer.php
@@ -5,6 +5,7 @@ namespace Drupal\tide_api\Plugin\jsonapi\FieldEnhancer;
 use Drupal\Core\Serialization\Yaml;
 use Drupal\jsonapi_extras\Plugin\ResourceFieldEnhancerBase;
 use Shaper\Util\Context;
+use Drupal\Component\Utility\Html;
 
 /**
  * Decode YAML content.
@@ -21,7 +22,9 @@ class YamlEnhancer extends ResourceFieldEnhancerBase {
    * {@inheritdoc}
    */
   protected function doUndoTransform($data, Context $context) {
-    return Yaml::decode($data);
+    $data = $this->processText($data);
+
+    return $data;
   }
 
   /**
@@ -45,6 +48,41 @@ class YamlEnhancer extends ResourceFieldEnhancerBase {
         ['type' => 'string'],
       ],
     ];
+  }
+
+  /**
+   * Helper function to convert node urls to path alias.
+   *
+   * @param string $data
+   *   Data from a webform.
+   *
+   * @return formattted data
+   */
+  public function processText($data) {
+    $formatted_data = Yaml::decode($data);
+    $processed_text = $formatted_data['processed_text']['#text'];
+    if (strpos($processed_text, 'data-entity-type') !== FALSE && strpos($processed_text, 'data-entity-uuid') !== FALSE) {
+      $dom = Html::load($processed_text);
+      $xpath = new \DOMXPath($dom);
+      foreach ($xpath->query('//a[@data-entity-type and @data-entity-uuid]') as $element) {
+        /** @var \DOMElement $element */
+        try {
+          $entity_type = $element->getAttribute('data-entity-type');
+          if ($entity_type == 'node') {
+            $href = $element->getAttribute('href');
+            $aliasByPath = \Drupal::service('path.alias_manager')->getAliasByPath($href);
+            $alias_helper = \Drupal::service('tide_site.alias_storage_helper');
+            $url = $alias_helper->getPathAliasWithoutSitePrefix(['alias' => $aliasByPath]);
+            $element->setAttribute('href', $url);
+          }
+        }
+        catch (\Exception $e) {
+          watchdog_exception('YamlEnhancer_processText', $e);
+        }
+      }
+      $formatted_data['processed_text']['#text'] = Html::serialize($dom);
+    }
+    return $formatted_data;
   }
 
 }
