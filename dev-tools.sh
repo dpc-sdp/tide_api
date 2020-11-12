4f03d809c0a0d6b4fb4b3f0a06d38d234402c9a2
Sonny Kieu <sonny@salsadigital.com.au>
2020-11-12 15:55:40 +1100

    Use PHP7.3 in dev-tools.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=a6b964d8a6a11cfbbb824b779c3a075187234cf1
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
